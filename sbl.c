d503309b9f5938df1b5a7c5d9bcb16340b9582ca
Mikhail Konovalov <mkonovalov@elvees.com>
2022-11-24 13:53:01 +0300

    pre-commit: Add clang-format hook
    This commit adds .clang-format from latest available tag [1].
    Modifications to .clang-format (see [2] for options doc):
    
    * Add 'AlignConsecutiveMacros: Consecutive'.
    * ColumnLimit is set to 100 instead of 80.
    
    [1] https://raw.githubusercontent.com/torvalds/linux/v6.0-rc6/.clang-format
    [2] https://clang.llvm.org/docs/ClangFormatStyleOptions.html
    
    Issue: #MCOM03SW-1807
    Change-Id: Idfedea221525ad69ab3c0861d7e96ce79b59def6

--- sbl.c
+++ sbl.c
@@ -5,66 +5,66 @@
 #include <string.h>
 #include "mips/m32c0.h"
 
-#define BIT(n) (1 << (n))
-#define GENMASK(hi, lo) (((1 << ((hi) - (lo) + 1)) - 1) << (lo))
-#define __bf_shf(x) (__builtin_ffs(x) - 1)
+#define BIT(n)			(1 << (n))
+#define GENMASK(hi, lo)		(((1 << ((hi) - (lo) + 1)) - 1) << (lo))
+#define __bf_shf(x)		(__builtin_ffs(x) - 1)
 #define FIELD_PREP(_mask, _val) ({ ((typeof(_mask))(_val) << __bf_shf(_mask)) & (_mask); })
-#define FIELD_GET(_mask, _reg) ({ (typeof(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask)); })
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#define FIELD_GET(_mask, _reg)	({ (typeof(_mask))(((_reg) & (_mask)) >> __bf_shf(_mask)); })
+#define ARRAY_SIZE(x)		(sizeof(x) / sizeof((x)[0]))
 
-#define COMM_UCG_CTR(i, j) (0xA1801000 + (i) * 0x1000 + (j) * 4)
-#define COMM_UCG_BP(i) (0xA1801040 + (i) * 0x1000)
-#define COMM_PLL 0xA1800000
+#define COMM_UCG_CTR(i, j) (0xA1801000 + (i)*0x1000 + (j)*4)
+#define COMM_UCG_BP(i)	   (0xA1801040 + (i)*0x1000)
+#define COMM_PLL	   0xA1800000
 
-#define PLL_CFG_SEL GENMASK(7, 0)
-#define PLL_CFG_MAN BIT(9)
-#define PLL_CFG_OD GENMASK(13, 10)
-#define PLL_CFG_NF GENMASK(26, 14)
-#define PLL_CFG_NR GENMASK(30, 27)
+#define PLL_CFG_SEL  GENMASK(7, 0)
+#define PLL_CFG_MAN  BIT(9)
+#define PLL_CFG_OD   GENMASK(13, 10)
+#define PLL_CFG_NF   GENMASK(26, 14)
+#define PLL_CFG_NR   GENMASK(30, 27)
 #define PLL_CFG_LOCK BIT(31)
 
-#define UCG_CTR_LPI_EN BIT(0)
-#define UCG_CTR_CLK_EN BIT(1)
-#define UCG_CTR_CLK_EN_STS GENMASK(4, 2)
+#define UCG_CTR_LPI_EN	       BIT(0)
+#define UCG_CTR_CLK_EN	       BIT(1)
+#define UCG_CTR_CLK_EN_STS     GENMASK(4, 2)
 #define UCG_CTR_QACTIVE_CTL_EN BIT(6)
-#define UCG_CTR_QFSM_STATE GENMASK(9, 7)
-#define UCG_CTR_DIV_COEFF GENMASK(29, 10)
-#define UCG_CTR_DIV_LOCK BIT(30)
+#define UCG_CTR_QFSM_STATE     GENMASK(9, 7)
+#define UCG_CTR_DIV_COEFF      GENMASK(29, 10)
+#define UCG_CTR_DIV_LOCK       BIT(30)
 
-#define SERV_URB_CPU_PPOLICY 0xBF000000
+#define SERV_URB_CPU_PPOLICY	       0xBF000000
 #define SERV_URB_HSPERIPH_SUBS_PPOLICY 0xBF000020
-#define SERV_URB_TOP_CLKGATE 0xBF001008
-#define SERV_WDT0_BASE 0xBF080000
-#define SERV_WDT_CR 0x0
-#define SERV_WDT_TORR 0x4
-#define SERV_WDT_CRR 0xC
-#define SERV_WDT_CRR_KICK_VALUE 0x76
-#define SERV_WDT_EN BIT(0)
+#define SERV_URB_TOP_CLKGATE	       0xBF001008
+#define SERV_WDT0_BASE		       0xBF080000
+#define SERV_WDT_CR		       0x0
+#define SERV_WDT_TORR		       0x4
+#define SERV_WDT_CRR		       0xC
+#define SERV_WDT_CRR_KICK_VALUE	       0x76
+#define SERV_WDT_EN		       BIT(0)
 
 #define SERV_RISC0_CSR 0xBFD08000
 
 #define CPU_CPU0_PPOLICY 0xA1000000
-#define CPU_SYS_PPOLICY 0xA1000040
-#define CPU_RVBADDR(x) (0xA1000118 + (x * 8))
-#define CPU_UCG_BYPASS 0xA1080040
-#define CPU_UCG_SYNC 0xA1080044
-#define CPU_UCG_CTR(i) (0xA1080000 + (i) * 4)
-#define CPU_PLL 0xA1000050
+#define CPU_SYS_PPOLICY	 0xA1000040
+#define CPU_RVBADDR(x)	 (0xA1000118 + (x * 8))
+#define CPU_UCG_BYPASS	 0xA1080040
+#define CPU_UCG_SYNC	 0xA1080044
+#define CPU_UCG_CTR(i)	 (0xA1080000 + (i)*4)
+#define CPU_PLL		 0xA1000050
 
 #define PP_ON 0x10
 
-#define TFA_MAGIC_ADDR 0xC0800000
+#define TFA_MAGIC_ADDR	0xC0800000
 #define TFA_MAGIC_VALUE 0xdeadc0de
 
 #define DDRINIT_START_ADDR_VIRT 0xA0000000
-#define TFA_START_ADDR_VIRT 0xC0300000
-#define TFA_START_ADDR_PHYS 0x880300000
-#define BL32_START_ADDR_VIRT 0xC1380000
-#define UBOOT_START_ADDR_VIRT 0xC0080000
+#define TFA_START_ADDR_VIRT	0xC0300000
+#define TFA_START_ADDR_PHYS	0x880300000
+#define BL32_START_ADDR_VIRT	0xC1380000
+#define UBOOT_START_ADDR_VIRT	0xC0080000
 
-#define WRITE_CPU_START_ADDR_REG(reg, val) \
-	do { \
-		writel((reg), ((uint64_t)(val)) >> 32); \
+#define WRITE_CPU_START_ADDR_REG(reg, val)                         \
+	do {                                                       \
+		writel((reg), ((uint64_t)(val)) >> 32);            \
 		writel((reg) + 4, ((uint64_t)(val)) & 0xffffffff); \
 	} while (0)
 
@@ -82,25 +82,25 @@ struct ucg_channel {
 };
 
 static struct ucg_channel comm_ucg_channels[] = {
-	{0, 0, 6},	/* DP		198 MHz */
-	{0, 1, 8},	/* VPU		148.5 MHz */
-	{0, 2, 4},	/* GPU		297 MHz */
-	{0, 3, 12},	/* ISP		99 MHz */
-	{0, 4, 4},	/* CPU		297 MHz */
-	{0, 5, 4},	/* ACP		297 MHz */
-	{0, 6, 12},	/* LSP0		99 MHz */
-	{0, 7, 4},	/* COH_COMM	297 MHz */
+	{ 0, 0, 6 }, /* DP		198 MHz */
+	{ 0, 1, 8 }, /* VPU		148.5 MHz */
+	{ 0, 2, 4 }, /* GPU		297 MHz */
+	{ 0, 3, 12 }, /* ISP		99 MHz */
+	{ 0, 4, 4 }, /* CPU		297 MHz */
+	{ 0, 5, 4 }, /* ACP		297 MHz */
+	{ 0, 6, 12 }, /* LSP0		99 MHz */
+	{ 0, 7, 4 }, /* COH_COMM	297 MHz */
 
 	/* To work around MCOM03SW-1192 the following frequency ratio must be met:
 	 * SLOW_COMM_FREQ < 1/2 * min(LSP0_SYS_FREQ, LSP1_SYS_FREQ, DDR_SYS_FREQ).
 	 */
-	{1, 0, 30},	/* SLOW_COMM	39.6 MHz */
-	{1, 2, 8},	/* FAST_COMM	148.5 MHz */
-	{1, 4, 4},	/* DSP		297 MHz */
-	{1, 5, 4},	/* PCIe		297 MHz */
-	{1, 6, 12},	/* LSP1		99 MHz */
-	{1, 7, 8},	/* SERVICE	148.5 MHz */
-	{1, 8, 6},	/* HSP		198 MHz */
+	{ 1, 0, 30 }, /* SLOW_COMM	39.6 MHz */
+	{ 1, 2, 8 }, /* FAST_COMM	148.5 MHz */
+	{ 1, 4, 4 }, /* DSP		297 MHz */
+	{ 1, 5, 4 }, /* PCIe		297 MHz */
+	{ 1, 6, 12 }, /* LSP1		99 MHz */
+	{ 1, 7, 8 }, /* SERVICE	148.5 MHz */
+	{ 1, 8, 6 }, /* HSP		198 MHz */
 };
 
 static inline void writel(unsigned long addr, uint32_t value)
@@ -113,7 +113,7 @@ static inline uint32_t readl(unsigned long addr)
 	return *(volatile uint32_t *)addr;
 }
 
-void* memcpy(void *dest, const void *src, size_t count)
+void *memcpy(void *dest, const void *src, size_t count)
 {
 	unsigned long *dl = (unsigned long *)dest, *sl = (unsigned long *)src;
 	char *d8, *s8;
@@ -122,7 +122,7 @@ void* memcpy(void *dest, const void *src, size_t count)
 		return dest;
 
 	/* While all data is aligned (common case), copy a word at a time */
-	if ( (((unsigned long)dest | (unsigned long)src) & (sizeof(*dl) - 1)) == 0) {
+	if ((((unsigned long)dest | (unsigned long)src) & (sizeof(*dl) - 1)) == 0) {
 		while (count >= sizeof(*dl)) {
 			*dl++ = *sl++;
 			count -= sizeof(*dl);
@@ -141,11 +141,8 @@ static void pll_cfg(unsigned long addr, int od, int nf)
 {
 	uint32_t val;
 
-	val = FIELD_PREP(PLL_CFG_SEL, 1) |
-	      FIELD_PREP(PLL_CFG_MAN, 1) |
-	      FIELD_PREP(PLL_CFG_OD, od) |
-	      FIELD_PREP(PLL_CFG_NF, nf) |
-	      FIELD_PREP(PLL_CFG_NR, 0);
+	val = FIELD_PREP(PLL_CFG_SEL, 1) | FIELD_PREP(PLL_CFG_MAN, 1) | FIELD_PREP(PLL_CFG_OD, od) |
+	      FIELD_PREP(PLL_CFG_NF, nf) | FIELD_PREP(PLL_CFG_NR, 0);
 	writel(addr, val);
 
 	while (!(readl(addr) & PLL_CFG_LOCK))
@@ -183,8 +180,7 @@ static void comm_ucg_cfg(void)
 
 	/* Set dividers */
 	for (int i = 0; i < ARRAY_SIZE(comm_ucg_channels); i++)
-		ucg_cfg(COMM_UCG_CTR(comm_ucg_channels[i].ucg_id,
-				     comm_ucg_channels[i].chan_id),
+		ucg_cfg(COMM_UCG_CTR(comm_ucg_channels[i].ucg_id, comm_ucg_channels[i].chan_id),
 			comm_ucg_channels[i].div);
 
 	/* Disable bypass */
@@ -207,7 +203,7 @@ static void start_arm_cpu(void)
 	uint8_t divs[] = {
 		4, /* sys clk 290.25 MHz */
 		1, /* core clk 1161 MHz */
-		2  /* dbus clk 580.5 MHz */
+		2 /* dbus clk 580.5 MHz */
 	};
 
 	/* Enable CPU_SUBS */
@@ -217,7 +213,7 @@ static void start_arm_cpu(void)
 	for (i = 1; i < ARRAY_SIZE(divs); i++) {
 		val = readl(CPU_UCG_CTR(i));
 		val |= UCG_CTR_CLK_EN;
-	        writel(CPU_UCG_CTR(i), val);
+		writel(CPU_UCG_CTR(i), val);
 		while (!(readl(CPU_UCG_CTR(i)) & UCG_CTR_DIV_LOCK))
 			continue;
 	}
@@ -301,7 +297,7 @@ int main(void)
 	start_arm_cpu();
 
 	while (1)
-		asm volatile ("wait");
+		asm volatile("wait");
 
 	return 0;
 }
