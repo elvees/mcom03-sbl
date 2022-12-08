0541e24ccb28e579f918e105e77e94cd74a782a8
Mikhail Konovalov <mkonovalov@elvees.com>
2022-12-08 18:35:45 +0300

    Enable MFBSP1, I2C4, SPIOTP, BPAM by default
    The SBL sets MFBSP1, I2C4, SPIOTP frequency to 49.5 MHz and BPAM
    frequency to 594 MHz.
    
    These settings are required as they were removed from U-Boot and
    Linux code.
    
    Issue: #MCOM03SW-2031
    Change-Id: I17ba13ab8f3303a869789f9c67181b52c3f91346

--- sbl.c
+++ sbl.c
@@ -90,15 +90,20 @@ static struct ucg_channel top_ucg_channels[] = {
 
 /* Service Subsystem PLL output frequency is 594 MHz, assuming that XTI = 27 MHz */
 static struct ucg_channel serv_ucg_channels[] = {
-	{ 1, SERV_UCG1_CHANNEL_CLK_CORE, 1 }, /* 594 MHz */
-	{ 1, SERV_UCG1_CHANNEL_CLK_RISC0, 1 }, /* 594 MHz */
-	{ 1, SERV_UCG1_CHANNEL_CLK_QSPI0, 1 }, /* 594 MHz */
-	{ 1, SERV_UCG1_CHANNEL_CLK_QSPI0_EXT, 22 }, /* 27 MHz */
 	{ 1, SERV_UCG1_CHANNEL_CLK_APB, 12 }, /* 49.5 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_CORE, 1 }, /* 594 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_QSPI0, 1 }, /* 594 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_BPAM, 1 }, /* 594 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_RISC0, 1 }, /* 594 MHz */
 	{ 1, SERV_UCG1_CHANNEL_CLK_MFBSP0, 12 }, /* 49.5 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_MFBSP1, 12 }, /* 49.5 MHz */
 	{ 1, SERV_UCG1_CHANNEL_CLK_MAILBOX0, 12 }, /* 49.5 MHz */
 	{ 1, SERV_UCG1_CHANNEL_CLK_PVTCTR, 12 }, /* 49.5 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_I2C4, 12 }, /* 49.5 MHz */
 	{ 1, SERV_UCG1_CHANNEL_CLK_TRNG, 12 }, /* 49.5 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_SPIOTP, 12 }, /* 49.5 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_I2C4_EXT, 12 }, /* 49.5 MHz */
+	{ 1, SERV_UCG1_CHANNEL_CLK_QSPI0_EXT, 22 }, /* 27 MHz */
 };
 
 /* CPU Subsystem PLL output frequency is 1161 MHz, assuming that XTI = 27 MHz */
