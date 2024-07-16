d93b5ee62ece345cbdb35e35e191c5f5938bcc97
Alexander Karnaukhov <akarnaukhov@elvees.com>
2024-07-16 11:58:00 +0300

    Port log
    This commit adapts log for sbl project:
    
    * remove unused header files;
    * remove U() macro when unnecessary;
    * remove ANSI escape codes in string definitions;
    * rename PLAT_LOG_LEVEL define to LOG_LEVEL;
    * add LOG_LEVEL definition in CMakeLists.txt for
      Release and Debug mode;
    * add guard for LOG_LEVEL define.
    
    Issue: #MCOM03SW-2603
    Change-Id: I11ecbc3682d27cd830240aff2fad3de1a88985b1

--- libs/log.h
+++ libs/log.h
@@ -1,16 +1,15 @@
 // SPDX-License-Identifier: MIT
-// Copyright 2023 RnD Center "ELVEES", JSC
+// Copyright 2023-2024 RnD Center "ELVEES", JSC
 
 #ifndef __LOG_H__
 #define __LOG_H__
 
-#include <lib/utils_def.h>
-#include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <sys/cdefs.h>
-#include <drivers/console/console.h>
-#include "platform_def.h"
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL LOG_LEVEL_NONE
+#endif
 
 /*
  * The log output macros print output to the console. These macros produce
@@ -23,23 +22,23 @@
  * WARN("Warning %s.\n", "message") -> WARNING: Warning message.
  */
 
-#define LOG_LEVEL_NONE	  U(0)
-#define LOG_LEVEL_ERROR	  U(10)
-#define LOG_LEVEL_NOTICE  U(20)
-#define LOG_LEVEL_WARNING U(30)
-#define LOG_LEVEL_INFO	  U(40)
-#define LOG_LEVEL_VERBOSE U(50)
+#define LOG_LEVEL_NONE    0
+#define LOG_LEVEL_ERROR   10
+#define LOG_LEVEL_NOTICE  20
+#define LOG_LEVEL_WARNING 30
+#define LOG_LEVEL_INFO    40
+#define LOG_LEVEL_VERBOSE 50
 
 /*
  * Define Log Markers corresponding to each log level which will
  * be embedded in the format string and is expected by printf() to determine
  * the log level.
  */
-#define LOG_MARKER_ERROR   "\033[41mERROR\033[00m: "
-#define LOG_MARKER_NOTICE  "\033[42mNOTICE\033[00m: "
-#define LOG_MARKER_WARNING "\033[43mWARN\033[00m: "
-#define LOG_MARKER_INFO	   "\033[42mINFO\033[00m: "
-#define LOG_MARKER_VERBOSE "\033[42mVERB\033[00m: "
+#define LOG_MARKER_ERROR   "ERROR: "
+#define LOG_MARKER_NOTICE  "NOTICE: "
+#define LOG_MARKER_WARNING "WARN: "
+#define LOG_MARKER_INFO    "INFO: "
+#define LOG_MARKER_VERBOSE "VERB: "
 
 /*
  * If the log output is too low then this macro is used in place of printf()
@@ -53,31 +52,31 @@
 		}                                     \
 	} while (false)
 
-#if PLAT_LOG_LEVEL >= LOG_LEVEL_ERROR
+#if LOG_LEVEL >= LOG_LEVEL_ERROR
 #define ERROR(fmt, ...) printf(LOG_MARKER_ERROR fmt, ##__VA_ARGS__)
 #else
 #define ERROR(fmt, ...) no_printf(LOG_MARKER_ERROR fmt, ##__VA_ARGS__)
 #endif
 
-#if PLAT_LOG_LEVEL >= LOG_LEVEL_NOTICE
+#if LOG_LEVEL >= LOG_LEVEL_NOTICE
 #define NOTICE(fmt, ...) printf(LOG_MARKER_NOTICE fmt, ##__VA_ARGS__)
 #else
 #define NOTICE(fmt, ...) no_printf(LOG_MARKER_NOTICE fmt, ##__VA_ARGS__)
 #endif
 
-#if PLAT_LOG_LEVEL >= LOG_LEVEL_WARNING
+#if LOG_LEVEL >= LOG_LEVEL_WARNING
 #define WARN(fmt, ...) printf(LOG_MARKER_WARNING fmt, ##__VA_ARGS__)
 #else
 #define WARN(fmt, ...) no_printf(LOG_MARKER_WARNING fmt, ##__VA_ARGS__)
 #endif
 
-#if PLAT_LOG_LEVEL >= LOG_LEVEL_INFO
+#if LOG_LEVEL >= LOG_LEVEL_INFO
 #define INFO(fmt, ...) printf(LOG_MARKER_INFO fmt, ##__VA_ARGS__)
 #else
 #define INFO(fmt, ...) no_printf(LOG_MARKER_INFO fmt, ##__VA_ARGS__)
 #endif
 
-#if PLAT_LOG_LEVEL >= LOG_LEVEL_VERBOSE
+#if LOG_LEVEL >= LOG_LEVEL_VERBOSE
 #define VERBOSE(fmt, ...) printf(LOG_MARKER_VERBOSE fmt, ##__VA_ARGS__)
 #else
 #define VERBOSE(fmt, ...) no_printf(LOG_MARKER_VERBOSE fmt, ##__VA_ARGS__)
