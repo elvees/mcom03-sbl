6b172d9a0f012ea2b44d3eb6e079e30df14ea408
Alexander Karnaukhov <akarnaukhov@elvees.com>
2024-07-17 11:02:30 +0300

    Port sbrk
    This commit adapts sbrk for sbl project:
    
    * substitute heap macros with variables;
    * add heap initialization function;
    * use C-style comments '//' for oneline comments;
    * update header paths and remove unused headers.
    
    Issue: #MCOM03SW-2603
    Change-Id: I465fa22fa2a5b5b6d205e2049b87dc6c893eb313

--- libs/platform/sbrk.c
+++ libs/platform/sbrk.c
@@ -1,42 +1,46 @@
 // SPDX-License-Identifier: MIT
-// Copyright 2023 RnD Center "ELVEES", JSC
+// Copyright 2023-2024 RnD Center "ELVEES", JSC
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
 #include <errno.h>
-#include <lib/utils_def.h>
-#include <platform_def.h>
+#include <stddef.h>
+#include <stdint.h>
 
-#define HEAP_START ((const char *)heap)
-#define HEAP_MAX   ((const char *)(HEAP_START + sizeof(heap)))
+#include <libs/platform-def-common.h>
+#include <libs/utils-def.h>
 
-static unsigned char heap[PLATFORM_HEAP_SIZE]
-	__attribute__((aligned(PLATFORM_HEAP_POINTER_ALIGN), section(".heap"))) = { 0 };
+static uintptr_t heap_start;
+static uintptr_t heap_end;
+static const char *heap_ptr;
+
+void heap_init(uintptr_t start, uintptr_t end)
+{
+	heap_start = start;
+	heap_end = end;
+
+	heap_ptr = (const char *)heap_start;
+}
 
 void *sbrk(ptrdiff_t size)
 {
-	/* heap_ptr is initialized to HEAP_START */
-	static const char *heap_ptr = HEAP_START;
 	const char *old_heap_ptr = NULL;
 
-	if (HEAP_START == HEAP_MAX) {
-		/* No Heap provided */
+	if (heap_start == heap_end) {
+		// No Heap provided
 		errno = ENOMEM;
 		return (void *)-1;
 	}
 
 	old_heap_ptr = heap_ptr;
-	size = ALIGN_UP(size, PLATFORM_HEAP_POINTER_ALIGN);
 
-	if ((heap_ptr + size) > HEAP_MAX) {
-		/* top of heap is bigger than HEAP_MAX */
+	size = ALIGN_UP(size, PLAT_HEAP_POINTER_ALIGN);
+
+	if ((uintptr_t)(heap_ptr + size) > heap_end) {
+		// top of heap is bigger than heap_max
 		errno = ENOMEM;
 		return (void *)-1;
 	}
 
-	/* success: update heap_ptr and return previous value */
+	// success: update heap_ptr and return previous value
 	heap_ptr += size;
 
 	return (void *)old_heap_ptr;
