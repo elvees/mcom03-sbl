6dcdba974df0885651ebe70d27120c03b227a39c
Alexander Karnaukhov <akarnaukhov@elvees.com>
2024-07-19 16:09:17 +0300

    Add platform-def-common
    This commit adds header file to be used across SBL components
    that contains common definitions for stack protection, heap
    alignment, IOMMU and subsystem frequencies.
    
    Issue: #MCOM03SW-2603
    Change-Id: I8fc01cd2205fd23376807b6b2e1dd31c66f66f76

---
+++ libs/platform-def-common.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2024 RnD Center "ELVEES", JSC
+
+#ifndef __PLATFORM_DEF_COMMON_H__
+#define __PLATFORM_DEF_COMMON_H__
+
+#include "utils-def.h"
+
+#define PLAT_RANDOM_CANARY_VALUE UL(0xBAADF00D)
+
+#define PLAT_HEAP_POINTER_ALIGN 16
+
+#define PLAT_IOMMU_BASE 0x40000000
+#define PLAT_IOMMU_SIZE 0x3000
+
+#define PLAT_IOMMU_VIRT_BASE_START 0xC0000000 // start of 2MiB slot section
+#define PLAT_IOMMU_VIRT_SLOT_COUNT 256 // count of 2MiB blocks in the section
+
+// Frequencies
+#define MCOM03_XTI_CLK_HZ (27 * _MHz_)
+#define HS_CLK_HZ         MCOM03_XTI_CLK_HZ
+#define QSPI1_CLK_HZ      MCOM03_XTI_CLK_HZ
+#define QSPI1_EXT_CLK_HZ  (MCOM03_XTI_CLK_HZ / 2)
+#define UART_CLK_HZ       MCOM03_XTI_CLK_HZ
+
+#endif // __PLATFORM_DEF_COMMON_H__
