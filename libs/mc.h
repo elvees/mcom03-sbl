110a8e7e95ec18e79d3f4c21f0007606fc4cf542
Alexander Karnaukhov <akarnaukhov@elvees.com>
2024-10-28 09:51:11 +0300

    Port RISC0 MC coprocessor helpers
    This commit adapts mc for sbl project:
    
    * reformat comments that consist of one line to "C++-style;
    * translate comments from Russian to English;
    * substitute header guards with #pragma once;
    
    Issue: #MCOM03SW-2603
    Change-Id: I859a3ad396a44485a0b5bc9d5d09aa5df4958a36

--- libs/mc.h
+++ libs/mc.h
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
-// Copyright 2023 RnD Center "ELVEES", JSC
+// Copyright 2023-2024 RnD Center "ELVEES", JSC
 
-#ifndef __MC_H__
-#define __MC_H__
+#pragma once
 
-/*
- * Read C0 coprocessor register.
- */
+// Read C0 coprocessor register.
 #define mips_read_c0_register(reg)                                             \
 	({                                                                     \
 		unsigned __value;                                              \
@@ -14,54 +11,46 @@
 		__value;                                                       \
 	})
 
-/*
- * Write C0 coprocessor register.
- */
+// Write C0 coprocessor register.
 #define mips_write_c0_register(reg, value)                                \
 	do {                                                              \
 		__asm__ volatile("mtc0   %z0, $%1 \n nop \n  nop \n  nop" \
-				 :                                        \
-				 : "r"((unsigned int)(value)), "K"(reg)); \
+		                 :                                        \
+		                 : "r"((unsigned int)(value)), "K"(reg)); \
 	} while (0)
 
-/*--------------------------------------
- * Coprocessor 0 registers.
- */
-#define C0_INDEX    0 /* индекс доступа к TLB */
-#define C0_RANDOM   1 /* индекс TLB для команды Write Random */
-#define C0_ENTRYLO0 2 /* строки для чётных страниц TLB */
-#define C0_ENTRYLO1 3 /* строки для нечётных страниц TLB */
-#define C0_CONTEXT  4 /* указатель на таблицу PTE */
-#define C0_PAGEMASK 5 /* маска размера страниц TLB */
-#define C0_WIRED    6 /* граница привязанных строк TLB */
-#define C0_BADVADDR 8 /* виртуальный адрес последнего исключения */
-#define C0_COUNT    9 /* таймер */
-#define C0_ENTRYHI  10 /* информация соответствия виртуального адреса */
-#define C0_COMPARE  11 /* предельное значение для прерывания по таймеру */
-#define C0_STATUS   12 /* режимы функционирования процессора */
-#define C0_CAUSE    13 /* причина последнего исключения */
-#define C0_EPC	    14 /* адрес возврата из исключения */
-#define C0_PRID	    15 /* идентификатор процессора */
-#define C0_CONFIG   16 /* информация о возможностях процессора */
-#define C0_LLADDR   17 /* физический адрес последней команды LL */
-#define C0_ERROREPC 30 /* адрес возврата из исключения ошибки */
+// Coprocessor 0 registers.
+#define C0_INDEX    0 // access index to TLB
+#define C0_RANDOM   1 // TBL index for Write Random command
+#define C0_ENTRYLO0 2 // strings for even TLB pages
+#define C0_ENTRYLO1 3 // strings for odd TLB pages
+#define C0_CONTEXT  4 // pointer to PTE table
+#define C0_PAGEMASK 5 // TLB page size mask
+#define C0_WIRED    6 // border of linked TLB strings
+#define C0_BADVADDR 8 // last exception virtual address
+#define C0_COUNT    9 // timer
+#define C0_ENTRYHI  10 // virtual address compliance information
+#define C0_COMPARE  11 // limit value for interrupt via timer
+#define C0_STATUS   12 // processor functional modes
+#define C0_CAUSE    13 // reason of the last exception
+#define C0_EPC      14 // return address from exception
+#define C0_PRID     15 // processor id
+#define C0_CONFIG   16 // processor abilities info
+#define C0_LLADDR   17 // phisical address of the last LL command
+#define C0_ERROREPC 30 // return address from exception error
 
-#define mips_get_core_id() (0)
+#define mips_get_core_id() 0
 
-/*
- * EXCEPTION TABLE
- */
-#define EXC_INT	 0 /* Int  Прерывание */
-#define EXC_MOD	 1 /* Mod  TLB-исключение модификации */
-#define EXC_TLBL 2 /* TLBL TLB-исключение (загрузка или вызов команды) */
-#define EXC_TLBS 3 /* TLBS TLB-исключение (сохранение) */
-#define EXC_ADEL 4 /* AdEL Прерывание по ошибке адресации (загрузка или вызов команды) */
-#define EXC_ADES 5 /* AdES Прерывание по ошибке адресации (сохранение) */
-#define EXC_SYS	 8 /* Sys  Системное  исключение */
-#define EXC_BP	 9 /* Bp   Исключение Breakpoint */
-#define EXC_RI	 10 /* RI   Исключение зарезервированной команды */
-#define EXC_CPU	 11 /* CpU  Исключение недоступности сопроцессора */
-#define EXC_OV	 12 /* Ov   Исключение целочисленного переполнения */
-#define EXC_TR	 13 /* Tr   Исключение Trap  */
-
-#endif /* __MC_H__ */
+// EXCEPTION TABLE
+#define EXC_INT  0 // Int  Interruption
+#define EXC_MOD  1 // Mod  TLB-exception of modification
+#define EXC_TLBL 2 // TLBL TLB-exception (loading or command call)
+#define EXC_TLBS 3 // TLBS TLB-exception (saving)
+#define EXC_ADEL 4 // AdEL Interruption due to an addressing error (loading or command call)
+#define EXC_ADES 5 // AdES Interruption due to an addressing error (saving)
+#define EXC_SYS  8 // Sys  System exception
+#define EXC_BP   9 // Bp   Breakpoint exception
+#define EXC_RI   10 // RI   Reserved command exception
+#define EXC_CPU  11 // CpU  Coprocessort unavailability exception
+#define EXC_OV   12 // Ov   Integer overflow exception
+#define EXC_TR   13 // Tr   Trap exception
