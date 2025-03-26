052aba4c643287c331aab897da3a2c7968d644ee
Alexander Karnaukhov <akarnaukhov@elvees.com>
2025-03-26 10:42:02 +0300

    Prepare snprintf.c and printf.c for code reuse
    The snprintf.c and printf.c files contain functions that share similar
    structures and functionality. This patch performs following formating
    in order to unify these elements:
    
    * explicitly add header files that provide used declarations and
      definitions;
    * create separate functions for the functionality that applies only
      to the corresponding files;
    * reformat comments that consist of one line to "C++-style;
    * for comments that consist of more than one line, it is used "C-style";
    * format common functions to single names, code style and interfaces;
    * complete the common functions that are lacking in functionality;
    
    Issue: #MCOM03SW-2603
    Change-Id: Ie83b5eb7a5306d659197f4aea5359c5127aa56b3

--- libs/platform/printf.c
+++ libs/platform/printf.c
@@ -2,8 +2,6 @@
 // Copyright 2023-2025 RnD Center "ELVEES", JSC
 
 #include <stdarg.h>
-#include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 
 #define get_num_va_args(_args, _lcount)                   \
@@ -15,24 +13,24 @@
 	                   (((_lcount) == 1) ? va_arg(_args, unsigned long int) : \
 	                                       va_arg(_args, unsigned int)))
 
-static int string_print(const char *str)
+static void char_print(size_t *chars_printed, const char ch)
 {
-	int count = 0;
-
-	for (; *str != '\0'; str++) {
-		(void)putchar(*str);
-		count++;
-	}
-
-	return count;
+	(void)putchar(ch);
+	(*chars_printed)++;
 }
 
-static int unsigned_num_print(unsigned long long int unum, unsigned int radix, char padc, int padn,
-                              unsigned char is_upper)
+static void string_print(size_t *chars_printed, const char *str)
+{
+	for (; *str != '\0'; str++)
+		char_print(chars_printed, *str);
+}
+
+static void unsigned_num_print(size_t *chars_printed, unsigned long long int unum,
+                               unsigned int radix, char padc, int padn, unsigned char is_upper)
 {
 	// Just need enough space to store 64 bit decimal integer
 	char num_buf[20];
-	int i = 0, count = 0;
+	int i = 0;
 	unsigned int rem;
 
 	do {
@@ -44,44 +42,19 @@ static int unsigned_num_print(unsigned long long int unum, unsigned int radix, c
 
 		i++;
 		unum /= radix;
-	} while (unum > 0U);
+	} while (unum > 0);
 
 	if (padn > 0)
 		while (i < padn) {
-			(void)putchar(padc);
-			count++;
+			char_print(chars_printed, padc);
 			padn--;
 		}
 
-	while (--i >= 0) {
-		(void)putchar(num_buf[i]);
-		count++;
-	}
-
-	return count;
+	while (--i >= 0)
+		char_print(chars_printed, num_buf[i]);
 }
 
-/**
- * Reduced format print for Trusted firmware.
- * The following type specifiers are supported by this print
- * %x - hexadecimal format
- * %s - string format
- * %d or %i - signed decimal format
- * %u - unsigned decimal format
- * %p - pointer format
- *
- * The following length specifiers are supported by this print
- * %l - long int (64-bit on AArch64)
- * %ll - long long int (64-bit on AArch64)
- * %z - size_t sized integer formats (64 bit on AArch64)
- *
- * The following padding specifiers are supported by this print
- * %0NN - Left-pad the number with 0s (NN is a decimal number)
- *
- * The print exits on all other formats specifiers other than valid
- * combinations of the above specifiers.
- */
-int vprintf(const char *fmt, va_list args)
+static int format_specifier_parser(const char *fmt, va_list args)
 {
 	int l_count;
 	long long int num;
@@ -89,7 +62,7 @@ int vprintf(const char *fmt, va_list args)
 	char *str;
 	char padc = '\0'; // Padding character
 	int padn; // Number of characters to pad
-	int count = 0; // Number of printed characters
+	size_t chars_printed = 0; // Number of printed characters
 
 	while (*fmt != '\0') {
 		l_count = 0;
@@ -100,44 +73,44 @@ int vprintf(const char *fmt, va_list args)
 			// Check the format specifier
 loop:
 			switch (*fmt) {
-			case 'i': // Fall through to next one
+			case 'i':
 			case 'd':
 				num = get_num_va_args(args, l_count);
 				if (num < 0) {
-					(void)putchar('-');
+					char_print(&chars_printed, '-');
+
 					unum = (unsigned long long int)-num;
 					padn--;
 				} else {
 					unum = (unsigned long long int)num;
 				}
 
-				count += unsigned_num_print(unum, 10, padc, padn, 0);
+				unsigned_num_print(&chars_printed, unum, 10, padc, padn, 0);
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				count += string_print(str);
+				string_print(&chars_printed, str);
 				break;
 			case 'c':
 				num = get_num_va_args(args, l_count);
-				(void)putchar(num);
-				count++;
+				char_print(&chars_printed, num);
 				break;
 			case 'p':
 				unum = (uintptr_t)va_arg(args, void *);
 				if (unum > 0U) {
-					count += string_print("0x");
+					string_print(&chars_printed, "0x");
 					padn -= 2;
 				}
 
-				count += unsigned_num_print(unum, 16, padc, padn, 0);
+				unsigned_num_print(&chars_printed, unum, 16, padc, padn, 0);
 				break;
 			case 'x':
 				unum = get_unum_va_args(args, l_count);
-				count += unsigned_num_print(unum, 16, padc, padn, 0);
+				unsigned_num_print(&chars_printed, unum, 16, padc, padn, 0);
 				break;
 			case 'X':
 				unum = get_unum_va_args(args, l_count);
-				count += unsigned_num_print(unum, 16, padc, padn, 1);
+				unsigned_num_print(&chars_printed, unum, 16, padc, padn, 1);
 				break;
 			case 'z':
 				if (sizeof(size_t) == 8U)
@@ -151,7 +124,7 @@ loop:
 				goto loop;
 			case 'u':
 				unum = get_unum_va_args(args, l_count);
-				count += unsigned_num_print(unum, 10, padc, padn, 0);
+				unsigned_num_print(&chars_printed, unum, 10, padc, padn, 0);
 				break;
 			case '0':
 				padc = '0';
@@ -162,6 +135,7 @@ loop:
 					char ch = *fmt;
 					if ((ch < '0') || (ch > '9'))
 						goto loop;
+
 					padn = (padn * 10) + (ch - '0');
 					fmt++;
 				}
@@ -172,22 +146,44 @@ loop:
 			fmt++;
 			continue;
 		}
-		(void)putchar(*fmt);
+
+		char_print(&chars_printed, *fmt);
 		fmt++;
-		count++;
 	}
 
-	return count;
+	return chars_printed;
 }
 
+/**
+ * Reduced format print.
+ * The following type specifiers are supported:
+ *
+ * %d or %i - signed decimal format
+ * %s - string format
+ * %c - character foramt
+ * %p - pointer format
+ * %x - hexadecimal format (lower case)
+ * %X - hexadecimal format (upper case)
+ * %u - unsigned decimal format
+ *
+ * The following length specifiers are supported
+ * %l - long int
+ * %ll - long long int
+ * %z - size_t sized integer formats
+ *
+ * The following padding specifiers are supported by this print
+ * %0NN - Left-pad the number with 0s (NN is a decimal number)
+ *
+ * The function exits on all other formats specifiers.
+ */
 int printf(const char *fmt, ...)
 {
-	int count;
-	va_list va;
+	int chars_printed; // Number of printed characters
+	va_list args;
 
-	va_start(va, fmt);
-	count = vprintf(fmt, va);
-	va_end(va);
+	va_start(args, fmt);
+	chars_printed = format_specifier_parser(fmt, args);
+	va_end(args);
 
-	return count;
+	return chars_printed;
 }
--- libs/platform/snprintf.c
+++ libs/platform/snprintf.c
@@ -1,43 +1,42 @@
 // SPDX-License-Identifier: MIT
-// Copyright 2023 RnD Center "ELVEES", JSC
+// Copyright 2023-2025 RnD Center "ELVEES", JSC
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <assert.h>
 #include <stdarg.h>
-#include <stdint.h>
+#include <stdio.h>
 
-/*
- * TODO: add formatted output for %.3s (example)
- */
+// TODO: add formatted output for %.3s (example)
 
 #define get_num_va_args(_args, _lcount)                   \
 	(((_lcount) > 1) ? va_arg(_args, long long int) : \
-			   (((_lcount) == 1) ? va_arg(_args, long int) : va_arg(_args, int)))
+	                   (((_lcount) == 1) ? va_arg(_args, long int) : va_arg(_args, int)))
 
 #define get_unum_va_args(_args, _lcount)                                          \
 	(((_lcount) > 1) ? va_arg(_args, unsigned long long int) :                \
-			   (((_lcount) == 1) ? va_arg(_args, unsigned long int) : \
-					       va_arg(_args, unsigned int)))
+	                   (((_lcount) == 1) ? va_arg(_args, unsigned long int) : \
+	                                       va_arg(_args, unsigned int)))
+
+static void char_print(char **s, size_t n, size_t *chars_printed, const char ch)
+{
+	if (*chars_printed < n) {
+		*(*s) = ch;
+		(*s)++;
+	}
+
+	(*chars_printed)++;
+}
 
 static void string_print(char **s, size_t n, size_t *chars_printed, const char *str)
 {
-	while (*str != '\0') {
-		if (*chars_printed < n) {
-			*(*s) = *str;
-			(*s)++;
-		}
-
-		(*chars_printed)++;
-		str++;
-	}
+	for (; *str != '\0'; str++)
+		char_print(s, n, chars_printed, *str);
 }
 
-static void unsigned_dec_print(char **s, size_t n, size_t *chars_printed,
-			       unsigned long long int unum, unsigned int radix, char padc, int padn)
+static void unsigned_num_print(char **s, size_t n, size_t *chars_printed,
+                               unsigned long long int unum, unsigned int radix, char padc, int padn,
+                               unsigned char is_upper)
 {
-	/* Enough for a 32-bit unsigned decimal integer (4294967295). */
-	char num_buf[10];
+	// Just need enough space to store 64 bit decimal integer
+	char num_buf[20];
 	int i = 0;
 	unsigned int rem;
 
@@ -46,85 +45,46 @@ static void unsigned_dec_print(char **s, size_t n, size_t *chars_printed,
 		if (rem < 0xa)
 			num_buf[i] = '0' + rem;
 		else
-			num_buf[i] = 'a' + (rem - 0xa);
+			num_buf[i] = (is_upper ? 'A' : 'a') + (rem - 0xa);
+
 		i++;
 		unum /= radix;
-	} while (unum > 0U);
+	} while (unum > 0);
 
-	if (padn > 0) {
+	if (padn > 0)
 		while (i < padn) {
-			if (*chars_printed < n) {
-				*(*s) = padc;
-				(*s)++;
-			}
-			(*chars_printed)++;
+			char_print(s, n, chars_printed, padc);
 			padn--;
 		}
-	}
 
-	while (--i >= 0) {
-		if (*chars_printed < n) {
-			*(*s) = num_buf[i];
-			(*s)++;
-		}
-		(*chars_printed)++;
-	}
+	while (--i >= 0)
+		char_print(s, n, chars_printed, num_buf[i]);
 }
 
-/*******************************************************************
- * Reduced snprintf to be used for Trusted firmware.
- * The following type specifiers are supported:
- *
- * %d or %i - signed decimal format
- * %s - string format
- * %u - unsigned decimal format
- *
- * The function panics on all other formats specifiers.
- *
- * It returns the number of characters that would be written if the
- * buffer was big enough. If it returns a value lower than n, the
- * whole string has been written.
- *******************************************************************/
-int snprintf(char *s, size_t n, const char *fmt, ...)
+static int format_specifier_parser(char **s, size_t n, const char *fmt, va_list args)
 {
-	va_list args;
 	int l_count;
 	long long int num;
 	unsigned long long int unum;
 	char *str;
-	char padc = '\0'; /* Padding character */
-	int padn; /* Number of characters to pad */
-	size_t chars_printed = 0U;
+	char padc = '\0'; // Padding character
+	int padn; // Number of characters to pad
+	size_t chars_printed = 0; // Number of printed characters
 
-	if (n == 0U) {
-		/* There isn't space for anything. */
-	} else if (n == 1U) {
-		/* Buffer is too small to actually write anything else. */
-		*s = '\0';
-		n = 0U;
-	} else {
-		/* Reserve space for the terminator character. */
-		n--;
-	}
-
-	va_start(args, fmt);
 	while (*fmt != '\0') {
 		l_count = 0;
 		padn = 0;
+
 		if (*fmt == '%') {
 			fmt++;
-			/* Check the format specifier. */
+			// Check the format specifier
 loop:
 			switch (*fmt) {
 			case 'i':
 			case 'd':
 				num = get_num_va_args(args, l_count);
 				if (num < 0) {
-					if (chars_printed < n) {
-						*s = '-';
-						s++;
-					}
-					chars_printed++;
+					char_print(s, n, &chars_printed, '-');
 
 					unum = (unsigned long long int)-num;
 					padn--;
@@ -132,28 +92,37 @@ loop:
 					unum = (unsigned long long int)num;
 				}
 
-				unsigned_dec_print(&s, n, &chars_printed, unum, 10, padc, padn);
+				unsigned_num_print(s, n, &chars_printed, unum, 10, padc, padn, 0);
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				string_print(&s, n, &chars_printed, str);
+				string_print(s, n, &chars_printed, str);
+				break;
+			case 'c':
+				num = get_num_va_args(args, l_count);
+				char_print(s, n, &chars_printed, num);
 				break;
 			case 'p':
 				unum = (uintptr_t)va_arg(args, void *);
 				if (unum > 0U) {
-					string_print(&s, n, &chars_printed, "0x");
+					string_print(s, n, &chars_printed, "0x");
 					padn -= 2;
 				}
 
-				unsigned_dec_print(&s, n, &chars_printed, unum, 16, padc, padn);
+				unsigned_num_print(s, n, &chars_printed, unum, 16, padc, padn, 0);
 				break;
 			case 'x':
 				unum = get_unum_va_args(args, l_count);
-				unsigned_dec_print(&s, n, &chars_printed, unum, 16, padc, padn);
+				unsigned_num_print(s, n, &chars_printed, unum, 16, padc, padn, 0);
+				break;
+			case 'X':
+				unum = get_unum_va_args(args, l_count);
+				unsigned_num_print(s, n, &chars_printed, unum, 16, padc, padn, 1);
 				break;
 			case 'z':
 				if (sizeof(size_t) == 8U)
 					l_count = 2;
+
 				fmt++;
 				goto loop;
 			case 'l':
@@ -162,7 +131,7 @@ loop:
 				goto loop;
 			case 'u':
 				unum = get_unum_va_args(args, l_count);
-				unsigned_dec_print(&s, n, &chars_printed, unum, 10, padc, padn);
+				unsigned_num_print(s, n, &chars_printed, unum, 10, padc, padn, 0);
 				break;
 			case '0':
 				padc = '0';
@@ -171,33 +140,76 @@ loop:
 
 				for (;;) {
 					char ch = *fmt;
-					if ((ch < '0') || (ch > '9')) {
+					if ((ch < '0') || (ch > '9'))
 						goto loop;
-					}
+
 					padn = (padn * 10) + (ch - '0');
 					fmt++;
 				}
 			default:
-				/* Panic on any other format specifier. */
-				assert(0); /* Unreachable */
+				// Exit on any other format specifier
+				return -1;
 			}
 			fmt++;
 			continue;
 		}
 
-		if (chars_printed < n) {
-			*s = *fmt;
-			s++;
-		}
-
+		char_print(s, n, &chars_printed, *fmt);
 		fmt++;
-		chars_printed++;
 	}
 
+	return chars_printed;
+}
+
+/**
+ * Reduced snprintf.
+ * The following type specifiers are supported:
+ *
+ * %d or %i - signed decimal format
+ * %s - string format
+ * %c - character foramt
+ * %p - pointer format
+ * %x - hexadecimal format (lower case)
+ * %X - hexadecimal format (upper case)
+ * %u - unsigned decimal format
+ *
+ * The following length specifiers are supported
+ * %l - long int
+ * %ll - long long int
+ * %z - size_t sized integer formats
+ *
+ * The following padding specifiers are supported by this print
+ * %0NN - Left-pad the number with 0s (NN is a decimal number)
+ *
+ * The function exits on all other formats specifiers.
+ *
+ * It returns the number of characters that would be written if the
+ * buffer was big enough. If it returns a value lower than n, the
+ * whole string has been written.
+ */
+int snprintf(char *s, size_t n, const char *fmt, ...)
+{
+	int chars_printed; // Number of printed characters
+	va_list args;
+
+	switch (n) {
+	case 0: // There isn't space for anything
+		break;
+	case 1: // Buffer is too small to actually write anything else
+		*s = '\0';
+		n = 0;
+		break;
+	default: // Reserve space for the terminator character
+		n--;
+	}
+
+	va_start(args, fmt);
+	chars_printed = format_specifier_parser(&s, n, fmt, args);
 	va_end(args);
 
-	if (n > 0U)
+	// Place a NULL character at the end of the buffer
+	if (n > 0)
 		*s = '\0';
 
-	return (int)chars_printed;
+	return chars_printed;
 }
