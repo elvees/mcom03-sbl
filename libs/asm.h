753d40c77f0ce92e62d8eec512ba3c350937df2e
Mikhail Konovalov <mkonovalov@elvees.com>
2025-09-10 17:18:28 +0300

    libs/asm.h: Remove unused assembler macros
    Issue: #MCOM03SW-3579
    Change-Id: I2dd39f4561414a3c8335785908c4d5614781908d

--- libs/asm.h
+++ libs/asm.h
@@ -89,44 +89,8 @@ a:;
 	.set  CONTEXT_HI,                       33
 	.set  CONTEXT_STATUS,                   34
 	.set  CONTEXT_PC,                       35
-	.set  CONTEXT_F0,                       36
-	.set  CONTEXT_F1,                       37
-	.set  CONTEXT_F2,                       38
-	.set  CONTEXT_F3,                       39
-	.set  CONTEXT_F4,                       40
-	.set  CONTEXT_F5,                       41
-	.set  CONTEXT_F6,                       42
-	.set  CONTEXT_F7,                       43
-	.set  CONTEXT_F8,                       44
-	.set  CONTEXT_F9,                       45
-	.set  CONTEXT_F10,                      46
-	.set  CONTEXT_F11,                      47
-	.set  CONTEXT_F12,                      48
-	.set  CONTEXT_F13,                      49
-	.set  CONTEXT_F14,                      50
-	.set  CONTEXT_F15,                      51
-	.set  CONTEXT_F16,                      52
-	.set  CONTEXT_F17,                      53
-	.set  CONTEXT_F18,                      54
-	.set  CONTEXT_F19,                      55
-	.set  CONTEXT_F20,                      56
-	.set  CONTEXT_F21,                      57
-	.set  CONTEXT_F22,                      58
-	.set  CONTEXT_F23,                      59
-	.set  CONTEXT_F24,                      60
-	.set  CONTEXT_F25,                      61
-	.set  CONTEXT_F26,                      62
-	.set  CONTEXT_F27,                      63
-	.set  CONTEXT_F28,                      64
-	.set  CONTEXT_F29,                      65
-	.set  CONTEXT_F30,                      66
-	.set  CONTEXT_F31,                      67
-	.set  CONTEXT_FCSR,                     68
-	.set  CONTEXT_FLAG_HAS_FPU,             69
-	.set  CONTEXT_FLAG_CRITICAL_NESTING,    70
-	.set  CONTEXT_FLAG_INTERRUPT_NESTING,   71
 
-	.set CONTEXT_WORDS,                     72
+	.set CONTEXT_WORDS,                     36
 
 	.set $CP0_INDEX,                        $0
 	.set $CP0_RANDOM,                       $1
@@ -174,7 +138,10 @@ a:;
 	.set TR_CRAM,                           0x2
 	.set TR_CRAM_MASK,                      0xFFFFFFFD
 
-	.macro SAVE_REGS
+/**
+ * This macro is used to save registers into stack
+ */
+.macro SAVE_REGS
 
 	addi	$sp, -CONTEXT_WORDS*4		// Allocate space for registers
 
@@ -224,10 +191,12 @@ a:;
 
 	mfc0	$k0, $CP0_STATUS
 	sw	$k0, (CONTEXT_STATUS * 4) ($sp)
-	.endm
+.endm
 
-
-	.macro RESTORE_REGS
+/**
+ * This macro is used to restore registers saved in stack
+ */
+.macro RESTORE_REGS
 	lw	$k0, (CONTEXT_LO * 4) ($sp)	// Load HI, LO registers
 	mtlo	$k0
 	lw	$k0, (CONTEXT_HI * 4) ($sp)
@@ -272,151 +241,12 @@ a:;
 
 	addi	$sp, CONTEXT_WORDS*4		// Deallocate saved context
 	.set pop
-	.endm
+.endm
 
-	.macro SAVE_FP_REGS
-
-// Already allocated space in SAVE_REGS macros
-
-	mfc1    $k0, $f0
-	sw	$k0, (CONTEXT_F0 * 4) ($sp)	// Save fp registers
-	mfc1    $k0, $f1
-	sw	$k0, (CONTEXT_F1 * 4) ($sp)
-	mfc1    $k0, $f2
-	sw	$k0, (CONTEXT_F2 * 4) ($sp)
-	mfc1    $k0, $f3
-	sw	$k0, (CONTEXT_F3 * 4) ($sp)
-	mfc1    $k0, $f4
-	sw	$k0, (CONTEXT_F4 * 4) ($sp)
-	mfc1    $k0, $f5
-	sw	$k0, (CONTEXT_F5 * 4) ($sp)
-	mfc1    $k0, $f6
-	sw	$k0, (CONTEXT_F6 * 4) ($sp)
-	mfc1    $k0, $f7
-	sw	$k0, (CONTEXT_F7 * 4) ($sp)
-	mfc1    $k0, $f8
-	sw	$k0, (CONTEXT_F8 * 4) ($sp)
-	mfc1    $k0, $f9
-	sw	$k0, (CONTEXT_F9 * 4) ($sp)
-	mfc1    $k0, $f10
-	sw	$k0, (CONTEXT_F10 * 4) ($sp)
-	mfc1    $k0, $f11
-	sw	$k0, (CONTEXT_F11 * 4) ($sp)
-	mfc1    $k0, $f12
-	sw	$k0, (CONTEXT_F12 * 4) ($sp)
-	mfc1    $k0, $f13
-	sw	$k0, (CONTEXT_F13 * 4) ($sp)
-	mfc1    $k0, $f14
-	sw	$k0, (CONTEXT_F14 * 4) ($sp)
-	mfc1    $k0, $f15
-	sw	$k0, (CONTEXT_F15 * 4) ($sp)
-	mfc1    $k0, $f16
-	sw	$k0, (CONTEXT_F16 * 4) ($sp)
-	mfc1    $k0, $f17
-	sw	$k0, (CONTEXT_F17 * 4) ($sp)
-	mfc1    $k0, $f18
-	sw	$k0, (CONTEXT_F18 * 4) ($sp)
-	mfc1    $k0, $f19
-	sw	$k0, (CONTEXT_F19 * 4) ($sp)
-	mfc1    $k0, $f20
-	sw	$k0, (CONTEXT_F20 * 4) ($sp)
-	mfc1    $k0, $f21
-	sw	$k0, (CONTEXT_F21 * 4) ($sp)
-	mfc1    $k0, $f22
-	sw	$k0, (CONTEXT_F22 * 4) ($sp)
-	mfc1    $k0, $f23
-	sw	$k0, (CONTEXT_F23 * 4) ($sp)
-	mfc1    $k0, $f24
-	sw	$k0, (CONTEXT_F24 * 4) ($sp)
-	mfc1    $k0, $f25
-	sw	$k0, (CONTEXT_F25 * 4) ($sp)
-	mfc1    $k0, $f26
-	sw	$k0, (CONTEXT_F26 * 4) ($sp)
-	mfc1    $k0, $f27
-	sw	$k0, (CONTEXT_F27 * 4) ($sp)
-	mfc1    $k0, $f28
-	sw	$k0, (CONTEXT_F28 * 4) ($sp)
-	mfc1    $k0, $f29
-	sw	$k0, (CONTEXT_F29 * 4) ($sp)
-	mfc1    $k0, $f30
-	sw	$k0, (CONTEXT_F30 * 4) ($sp)
-	mfc1    $k0, $f31
-	sw	$k0, (CONTEXT_F31 * 4) ($sp)
-	cfc1    $k0, $31
-	sw	$k0, (CONTEXT_FCSR * 4) ($sp)
-	.endm
-
-	.macro RESTORE_FP_REGS
-
-	lw	$k0, (CONTEXT_F0 * 4) ($sp)	// Restore fp registers
-	mtc1    $k0, $f0
-	lw	$k0, (CONTEXT_F1 * 4) ($sp)
-	mtc1    $k0, $f1
-	lw	$k0, (CONTEXT_F2 * 4) ($sp)
-	mtc1    $k0, $f2
-	lw	$k0, (CONTEXT_F3 * 4) ($sp)
-	mtc1    $k0, $f3
-	lw	$k0, (CONTEXT_F4 * 4) ($sp)
-	mtc1    $k0, $f4
-	lw	$k0, (CONTEXT_F5 * 4) ($sp)
-	mtc1    $k0, $f5
-	lw	$k0, (CONTEXT_F6 * 4) ($sp)
-	mtc1    $k0, $f6
-	lw	$k0, (CONTEXT_F7 * 4) ($sp)
-	mtc1    $k0, $f7
-	lw	$k0, (CONTEXT_F8 * 4) ($sp)
-	mtc1    $k0, $f8
-	lw	$k0, (CONTEXT_F9 * 4) ($sp)
-	mtc1    $k0, $f9
-	lw	$k0, (CONTEXT_F10 * 4) ($sp)
-	mtc1    $k0, $f10
-	lw	$k0, (CONTEXT_F11 * 4) ($sp)
-	mtc1    $k0, $f11
-	lw	$k0, (CONTEXT_F12 * 4) ($sp)
-	mtc1    $k0, $f12
-	lw	$k0, (CONTEXT_F13 * 4) ($sp)
-	mtc1    $k0, $f13
-	lw	$k0, (CONTEXT_F14 * 4) ($sp)
-	mtc1    $k0, $f14
-	lw	$k0, (CONTEXT_F15 * 4) ($sp)
-	mtc1    $k0, $f15
-	lw	$k0, (CONTEXT_F16 * 4) ($sp)
-	mtc1    $k0, $f16
-	lw	$k0, (CONTEXT_F17 * 4) ($sp)
-	mtc1    $k0, $f17
-	lw	$k0, (CONTEXT_F18 * 4) ($sp)
-	mtc1    $k0, $f18
-	lw	$k0, (CONTEXT_F19 * 4) ($sp)
-	mtc1    $k0, $f19
-	lw	$k0, (CONTEXT_F20 * 4) ($sp)
-	mtc1    $k0, $f20
-	lw	$k0, (CONTEXT_F21 * 4) ($sp)
-	mtc1    $k0, $f21
-	lw	$k0, (CONTEXT_F22 * 4) ($sp)
-	mtc1    $k0, $f22
-	lw	$k0, (CONTEXT_F23 * 4) ($sp)
-	mtc1    $k0, $f23
-	lw	$k0, (CONTEXT_F24 * 4) ($sp)
-	mtc1    $k0, $f24
-	lw	$k0, (CONTEXT_F25 * 4) ($sp)
-	mtc1    $k0, $f25
-	lw	$k0, (CONTEXT_F26 * 4) ($sp)
-	mtc1    $k0, $f26
-	lw	$k0, (CONTEXT_F27 * 4) ($sp)
-	mtc1    $k0, $f27
-	lw	$k0, (CONTEXT_F28 * 4) ($sp)
-	mtc1    $k0, $f28
-	lw	$k0, (CONTEXT_F29 * 4) ($sp)
-	mtc1    $k0, $f29
-	lw	$k0, (CONTEXT_F30 * 4) ($sp)
-	mtc1    $k0, $f30
-	lw	$k0, (CONTEXT_F31 * 4) ($sp)
-	mtc1    $k0, $f31
-	lw	$k0, (CONTEXT_FCSR * 4) ($sp)
-	ctc1    $k0, $31
-	.endm
-
-	.macro INIT_REGS
+/**
+ * This macro is used to initialize MIPS registers
+ */
+.macro INIT_REGS
 	.set	push
 	.set	noat
 	move	$AT, $zero
@@ -445,33 +275,18 @@ a:;
 	move	$t8, $zero
 	move	$t9, $zero
 	.set    pop
-	.endm
+.endm
 
-	.macro PUSH reg
-	addiu $sp, $sp, -16
-	sw  \reg, 12($sp)
-	sw  $fp, 8($sp)
-	move  $fp, $sp
-	.endm
-
-	.macro POP reg
-	lw \reg, 12($sp)
-	lw $fp, 8($sp)
-	addiu $sp, $sp, 16
-	.endm
-
-	/**
-	 * This macro sets the stack pointer to stack memory allocated in linker script and
-	 * stack check guard to pseudo random value.
-	 *
-	 * Note: Ideally, a random number should be assigned instead of the combination of a
-	 * timer's value and a compile-time constant. As currently the random number generator
-	 * has not implemented yet in the software, this is better than nothing but
-	 * not necessarily really secure.
-	 *
-	 * return $v0 with saved stack pointer
-	 */
-	.macro INIT_STACK
+/**
+ * This macro sets the stack pointer to stack memory allocated in linker script and
+ * stack check guard to pseudo random value.
+ *
+ * Note: Ideally, a random number should be assigned instead of the combination of a
+ * timer's value and a compile-time constant. As currently the random number generator
+ * has not implemented yet in the software, this is better than nothing but
+ * not necessarily really secure.
+ */
+.macro INIT_STACK
 	nop
 	li      $k0, PLAT_RANDOM_CANARY_VALUE
 	mfc0    $k1, $CP0_COUNT
@@ -482,29 +297,8 @@ a:;
 	sw      $k0, 0($k1)
 
 	// Set stack pointer
-	move    $v0, $sp
 	la      $sp, __stack
 	nop
-	.endm
+.endm
 
-	/**
-	 * In some cases it required to save predecessor's stack to restore it before jump back
-	 * to predecessor. This macro saves the stack pointer returned by INIT_STACK to
-	 * current stack.
-	 */
-	.macro SAVE_PRE_STACK
-	nop
-	PUSH    $v0
-	nop
-	.endm
-
-	/**
-	 * This macro restores the stack pointer returned by INIT_STACK from
-	 * current stack.
-	 */
-	.macro RESTORE_PRE_STACK
-	POP     $k0
-	move    $sp, $k0
-	nop
-	.endm
 #endif // __ASSEMBLER__
