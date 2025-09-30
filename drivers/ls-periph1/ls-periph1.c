cfda473ec303a546f730c4913fb7c89ea2c77e04
Mikhail Konovalov <mkonovalov@elvees.com>
2025-09-30 20:25:46 +0300

    Set LSP1 PLL and UCG default frequency
    We are going to use LSP1 TIMERS0 as system timer. The LSP1 TIMERS0
    frequency has to be fixed across boot stages. The PLL coefficients
    and UCG dividers are chosen to the ones set in our Linux Kernel.
    As PLL has to be tuned, the related clocks of peripherals used in
    firmware are tuned too. The resulting frequencies are the following:
    
          lsperiph1_pll     614250000
            lsp1_sys_clk    87750000
            timers0_clk     87750000
            uart0_clk       26706522
            gpio1_dbclk     1050000
    
    Issue: #MCOM03SW-3347
    Change-Id: I87dfe47ec20006371bf8a47ee2bd30dc567a8e62

--- drivers/ls-periph1/ls-periph1.c
+++ drivers/ls-periph1/ls-periph1.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include <drivers/gpio/gpio.h>
+#include <drivers/pll/pll.h>
 #include <drivers/service/service.h>
 #include <drivers/ucg/ucg.h>
 #include <libs/errors.h>
@@ -12,24 +13,45 @@
 
 #include "ls-periph1.h"
 
+// LSP1 Subsystem PLL output frequency is 614.250 MHz, assuming that XTI = 27 MHz
+static struct ucg_channel lsp1_ucg_channels[] = {
+	{ 0, LSP1_UCG_CLK_SYS, 7 }, // 87.750 MHz
+	{ 0, LSP1_UCG_CLK_GPIO1, 585 }, //  1.050 MHz
+	{ 0, LSP1_UCG_CLK_UART0, 23 }, // ~26.71 MHz
+	{ 0, LSP1_UCG_CLK_TIMERS, 7 }, // 87.750 MHz
+};
+
 static int lsp1_cfg_clks(void)
 {
 	int ret;
+
 	ucg_regs_t *ucg = ucg_get_registers(UCG_SUBSYS_LSP1, 0);
+	lsp1_urb_regs_t *urb = lsp1_get_urb_registers();
 
 	ret = ucg_enable_bp(ucg, LSP1_UCG_ALL_CH_MASK);
 	if (ret)
 		return ret;
 
-	// Initialize the UCG register for clocking GPIO1
-	ret = ucg_set_divider(ucg, LSP1_UCG_CLK_GPIO1, 1, 1000);
+	/**
+	 * Setup PLL to 614.250 MHz, assuming that XTI = 27 MHz. Use NR = 0 to
+	 * minimize PLL output jitter.
+	 */
+	pll_cfg_t pll_cfg;
+	pll_cfg.nf_value = 90;
+	pll_cfg.nr_value = 0;
+	pll_cfg.od_value = 3;
+
+	ret = pll_set_manual_freq((pll_cfg_reg_t *)&urb->pll_cfg, &pll_cfg, 1000);
 	if (ret)
 		return ret;
 
-	// Initialize the UCG register for clocking UART0
-	ret = ucg_set_divider(ucg, LSP1_UCG_CLK_UART0, 1, 1000);
-	if (ret)
-		return ret;
+	// Set dividers
+	for (int i = 0; i < ARRAY_SIZE(lsp1_ucg_channels); i++) {
+		ret = ucg_set_divider(ucg, lsp1_ucg_channels[i].chan_id, lsp1_ucg_channels[i].div,
+		                      1000);
+		if (ret)
+			return ret;
+	}
 
 	return ucg_sync_and_disable_bp(ucg, LSP1_UCG_ALL_CH_MASK, LSP1_UCG_SYNC_MASK);
 }
